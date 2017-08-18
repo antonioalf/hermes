a1fc75282075d80e3cd0aae7b16666350ae8f716
Frannie-Ludmilla <francesca.pesci.fl@gmail.com>
2017-08-18 12:16:09 +0200

    Added IP and Log

--- docker/docker-entrypoint-dcos.sh
+++ docker/docker-entrypoint-dcos.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -xe
 
 if [ $SEED = "true" ]; then
-    HOSTNAME="$(hostname -f)"
-    SEED_IP_ADDRESS="$(dig +short $HOSTNAME)"
+    if [ ! -z $SEED_IP ]; then
+        SEED_IP_ADDRESS=$SEED_IP
+    else
+        HOSTNAME="$(hostname)"
+        SEED_IP_ADDRESS="$(dig +short $HOSTNAME)"
+    fi
     java -jar -Dkhermes.ws=true -Dakka.remote.netty.tcp.port=$PORT0 -Dakka.remote.netty.tcp.hostname=$SEED_IP_ADDRESS -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@$SEED_IP_ADDRESS:$PORT0 -Dzookeeper.connection=master.mesos:2181 /khermes.jar
 else
-    screen -S client -d -m java -jar -Dkhermes.client=true -Dkhermes.ws=false -Dakka.remote.netty.tcp.port=$PORT0 -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@$SEED_IP:$SEED_PORT -Dzookeeper.connection=master.mesos:2181 /khermes.jar
+    java -jar -Dkhermes.client=true -Dkhermes.ws=false -Dakka.remote.netty.tcp.port=$PORT0 -Dakka.cluster.seed-nodes.0=akka.tcp://khermes@$SEED_IP:$SEED_PORT -Dzookeeper.connection=master.mesos:2181 /khermes.jar
 fi
 
-#tail -F /khermes.log
+tail -F /khermes.log
